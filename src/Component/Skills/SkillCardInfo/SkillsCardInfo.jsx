a43e9332043844f48b1b68ae515936ca5f0a7e16
Piyush Tak <piyushtak333@gmail.com>
2024-02-23 18:34:42 +0530

    changes

--- src/Component/Skills/SkillCardInfo/SkillsCardInfo.jsx
+++ src/Component/Skills/SkillCardInfo/SkillsCardInfo.jsx
@@ -5,6 +5,7 @@ const SkillsCardInfo = ({ heading, skills }) => {
   return (
     <div id="skills-info-card">
       <h6>{heading}</h6>
+      
       <div id="skills-info-content">
         {skills.map((item, index) => (
           <React.Fragment key={`skill_${index}`}>
@@ -12,6 +13,7 @@ const SkillsCardInfo = ({ heading, skills }) => {
               <p>{item.skill}</p>
               <p id="percentage">{item.percentage}</p>
             </div>
+
             <div id="skills-progress-bg">
               <div id="skills-progress" style={{ width: item.percentage }} />
             </div>
